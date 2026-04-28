cfd74ca65d35f18c6dea2400c206e77536aad3df
Spencer Peck <speck34@bruinmail.com>
2026-04-27 20:56:59 -0600

    psedou code

--- src/main/Deck.java
+++ src/main/Deck.java
@@ -12,6 +12,15 @@ public class Deck {
     public Deck(ArrayList<Card> cards) {
         drawPile = cards;
         discardPile = new ArrayList<>();
+        //TODO Change this constructor to accept a file path to read in a chance file or community chest file
+        //TODO Create method to read in strings
+        	//This is the new method pseduoCode
+        	//Read in next line
+        	//If matches a card text that is a get out of jail create jail card
+        	//if matches card text that is a move to location create a new MoveToCard with an extra parameter which is destination
+        	//if go to nearest railroad/utility create new nearestRailroad/Utillity Card
+        	//If go back three tiles create new gobackCard
+        	//If not match other requirements create new Card (aka no functionality required)
     }
 
     /**
